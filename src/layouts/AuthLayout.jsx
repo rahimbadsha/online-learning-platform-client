31e5e17386ff3a6515ce0d97b8d95c8145519524
Rahim <rahimbadsha.cse@gmail.com>
2025-11-11 21:47:17 +0600

    update authlayout.jsx

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Footer';
 
 const AuthLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
